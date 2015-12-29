57bbe0592067bec7faddac8cefa68c6cae9d86e1
Dr. Root <drrootprofessor@gmail.com>
2015-12-29 18:28:41 -0500

    Update ps1.py

--- ps1.py
+++ ps1.py
@@ -1,1 +1,11 @@
+== Exercises ==
+#Write a program that asks the user to type in a string, and then tells the user how long that string was.
+#Ask the user for a string, and then for a number. Print out that string, that many times. (For example, if the string is <code>hello</code> and the number is <code>3</code> you should print out <code>hellohellohello</code>.)
+#What would happen if a mischievous user typed in a word when you ask for a number? Try it.
+
+[[Python Programming/Variables and Strings/Solutions|Solutions]]
+
+<span style="font-size: 14pt;">[[Python Programming/Quiz 2|Quiz]]</span>
+
+{{Python Programming/Navigation|Creating Python programs|Basic Syntax}}
 
