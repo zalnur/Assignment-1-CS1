37cefb6653c36ee9c0a533f4f42591a7aaf5debb
Yousef Hafez <46639866+zalnur@users.noreply.github.com>
2019-01-16 11:25:28 +0800

    Done ps1.py
    Assignment 1
    Name: Zalnur
    Time Spent: 0:10

--- ps1.py
+++ ps1.py
@@ -1,2 +1,6 @@
+#!/usr/local/Cellar/python@2/2.7.15_2/Frameworks/Python.framework/Versions/2.7
 
+date = raw_input('Enter your date of birth: ')
+name = raw_input('Enter your last name: ')
 
+print name, date
